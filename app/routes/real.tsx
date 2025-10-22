acfc8e3b5df28814f4b960d54290640b8cb9297e
BrandonAlanDev <brandoncarabajal@gmail.com>
2025-10-22 11:12:10 -0300

    routes

--- app/routes/real.tsx
+++ app/routes/real.tsx
@@ -1,7 +1,8 @@
-import BackgroundArrows from "~/components/BackgroundArrows";
-import Navbar from "~/components/Navbar";
+import BackgroundArrows from "../components/BackgroundArrows";
+import SteamCostCalculator from "../components/SteamCostCalculator";
+import Navbar from "../components/Navbar";
 
-export function Welcome() {
+export default function Real() {
   return (
     <main className="flex items-center justify-center pt-[80px] pb-4">
       <BackgroundArrows/>
@@ -9,7 +10,8 @@ export function Welcome() {
       <div className="flex flex-col items-center gap-16 min-h-0 z-3 p-4 rounded-4xl border-2 bg-linear-60 bg-fixed from-black to-[#00032cbe] dark:from-[#ffffff] dark:to-[#f1fcffcc]  text-white dark:text-black drop-shadow-2xl shadow-xl shadow-black dark:shadow-cyan-50 dark:shadow-lg">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
-            <h1 className="text-4xl">Cuánto sale?</h1>
+            <h1 className="text-4xl">Real</h1>
+            <SteamCostCalculator/>
           </div>
         </header>
       </div>
