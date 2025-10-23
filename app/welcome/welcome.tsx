c68a40a1caed7b476994886d1079a4cd0b54a0a4
BrandonAlanDev <brandoncarabajal@gmail.com>
2025-10-23 16:23:38 -0300

    remove view changes

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -6,7 +6,7 @@ export function Welcome() {
     <main className="flex items-center justify-center pt-[80px] pb-4">
       <BackgroundArrows/>
       <Navbar/>
-      <div className="flex flex-col items-center gap-16 min-h-0 z-3 p-4 rounded-4xl border-2 bg-linear-60 bg-fixed from-black to-[#00032cbe] dark:from-[#ffffff] dark:to-[#f1fcffcc]  text-white dark:text-black drop-shadow-2xl shadow-xl shadow-black dark:shadow-cyan-50 dark:shadow-lg">
+      <div className="casillero flex flex-col items-center gap-16 min-h-0 z-3 p-4 rounded-4xl border-2 bg-linear-60 bg-fixed from-black to-[#00032cbe] dark:from-[#ffffff] dark:to-[#f1fcffcc]  text-white dark:text-black drop-shadow-2xl shadow-xl shadow-black dark:shadow-cyan-50 dark:shadow-lg">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
             <h1 className="text-4xl">Cuánto sale?</h1>
