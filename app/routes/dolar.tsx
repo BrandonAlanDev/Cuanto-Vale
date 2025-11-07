5c4db0e1bdd6e4826c193bb24f1baf083100a887
BrandonAlanDev <brandoncarabajal@gmail.com>
2025-11-06 21:57:12 -0300

    ads fix

--- app/routes/dolar.tsx
+++ app/routes/dolar.tsx
@@ -22,6 +22,7 @@ export default function Dolar() {
     <main className="flex items-center justify-center pt-[80px] pb-4">
       <div className="w-full lg:max-w-[50vw] casillero flex flex-col items-center gap-16 min-h-0 z-3 p-4 rounded-4xl border-2 bg-linear-60 bg-fixed from-black to-[#00032cbe] dark:from-[#ffffff] dark:to-[#f1fcffcc]  text-white dark:text-black drop-shadow-2xl shadow-xl shadow-black dark:shadow-cyan-50 dark:shadow-lg">
         <header className="w-full flex flex-col items-center gap-9">
+          <h2 className="text-xl font-bold mb-4">Precio del dolar en Pesos Argentinos (ARS)</h2>
           <div className="w-full lg:max-w-[50vw] overflow-x-auto p-4 select-none">
             <table className="w-full border-collapse text-center select-none rounded-2xl overflow-hidden">
               <thead className="bg-blue-950 text-white dark:bg-white dark:text-blue-950 text-2xl">
